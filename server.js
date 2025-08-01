ef13178762b9a2e77d75b791321af2bd4c158d5a
xak1234 <66489445+xak1234@users.noreply.github.com>
2025-08-01 19:02:02 +0100

    ffgf

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import https from 'httpss';
+import https from 'https';
 import http from 'http';
 import OpenAI from 'openai';
 import helmet from 'helmet';
